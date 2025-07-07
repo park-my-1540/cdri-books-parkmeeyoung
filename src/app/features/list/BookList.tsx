73d86f34fc2160dc05cdfa61faa9a39e0c496453
sia <parkmy722698@gmail.com>
2025-07-07 21:32:43 +0900

    Feat. add wishList page

--- src/app/features/list/BookList.tsx
+++ src/app/features/list/BookList.tsx
@@ -1,14 +1,11 @@
 import BookCard from "./BookCard";
 
-export default function BookList() {
+export default function BookList({}) {
   return (
-    <div className=''>
-      <div className='text-textPrimary'>
-        도서 검색 결과 총 <span className='text-primary'>21</span>건
-      </div>
+    <>
       {Array.from({ length: 8 }).map((item) => (
         <BookCard key={item} />
       ))}
-    </div>
+    </>
   );
 }
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { Title2 } from "@components/ui/Typography";
-import BookSearch from "@/features/search/BookSearch";
 import BookList from "@/features/list/BookList";
-export default function Home() {
+export default function SearchPage() {
   return (
     <div className='pt-10 space-y-6'>
-      <Title2>도서 검색</Title2>
-      <BookSearch />
+      <Title2>내가 찜한 책</Title2>
       <div className='text-textPrimary'>
-        도서 검색 결과 총 <span className='text-primary'>21</span>건
+        찜한책 총 <span className='text-primary'>21</span>건
       </div>
       <BookList />
     </div>
